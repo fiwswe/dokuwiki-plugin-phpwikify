facfab69edca7c5451ff7edcdb3981fe3bd9ebfa
trailjeep <trailjeep@gmail.com>
2020-06-05 20:37:58 +0000

    cleanup code a little

--- syntax.php
+++ syntax.php
@@ -10,49 +10,27 @@ if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../')
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'syntax.php');
 
-class syntax_plugin_phpwikify extends DokuWiki_Syntax_Plugin
-{
-	function syntax_plugin_phpwikify(){
-		global $PARSER_MODES;
-		$this->allowedModes = $PARSER_MODES['formatting'];
-	}
-	
-	function getType(){
-		return "protected";
-	}
+class syntax_plugin_phpwikify extends DokuWiki_Syntax_Plugin {
+    function syntax_plugin_phpwikify() { global $PARSER_MODES; $this->allowedModes = $PARSER_MODES['formatting']; }
+    function getType()  { return "protected"; }
+    function getPType() { return "normal"; }
+    function getSort()  { return 0; }
 
-	function getPType(){
-		return "normal";
-	}
-
-	function getSort(){
-		return 0;
-	}
-
-	function connectTo( $mode ) {
-        	$this->Lexer->addEntryPattern("<phpwikify>(?=.*?</phpwikify>)",$mode,"plugin_phpwikify");
-	}
- 
-	function postConnect() {
-		$this->Lexer->addExitPattern( "</phpwikify>","plugin_phpwikify");
-	}
- 
-	function handle( $match, $state, $pos, Doku_Handler $handler ){
-		return array($state,$match);
-	}
-
-	function render( $mode, Doku_Renderer $renderer, $data ) {
-		if($mode == 'xhtml'){
-			list($state, $data) = $data;
-			if ($state === DOKU_LEXER_UNMATCHED) {
-				ob_start();
-				eval( $data );
-				$renderer->doc .= p_render( "xhtml", p_get_instructions( ob_get_contents() ), $info );
-				ob_end_clean();
-			}
-			return true;
-		}
-		return false;
-	}
+    function connectTo( $mode ) { $this->Lexer->addEntryPattern("<phpwikify>(?=.*?</phpwikify>)",$mode,"plugin_phpwikify"); }
+    function postConnect() { $this->Lexer->addExitPattern( "</phpwikify>","plugin_phpwikify"); }
+    function handle( $match, $state, $pos, Doku_Handler $handler ){ return array($state,$match); }
+    function render( $mode, Doku_Renderer $renderer, $data ) {
+        if($mode == 'xhtml'){
+            list($state, $data) = $data;
+            if ($state === DOKU_LEXER_UNMATCHED) {
+                ob_start();
+                eval( $data );
+                $renderer->doc .= p_render( "xhtml", p_get_instructions( ob_get_contents() ), $info );
+                ob_end_clean();
+            }
+            return true;
+        }
+        return false;
+    }
 }
-?>
+//Setup VIM: ex: et ts=4 enc=utf-8 :
